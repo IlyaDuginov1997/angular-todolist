1b64df2e16104aa2cd426948cad7dffe84a67386
Ilya <IlyaDuginov1997@gmail.com>
2023-03-12 18:02:39 +0300

    feat: add unsubscriptions in the component. (Sun Mar 12 18:02:39 BST 2023)

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,54 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Todo, TodosService } from 'src/app/services/todos.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'inst-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss'],
 })
-export class TodosComponent implements OnInit {
+export class TodosComponent implements OnInit, OnDestroy {
   constructor(private todoService: TodosService) {}
 
   todos: Todo[] = [];
   error = '';
+  subscription: Subscription = new Subscription();
 
   ngOnInit(): void {
     this.getTodos();
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
   getTodos() {
-    this.todoService.getTodos().subscribe({
-      next: res => {
-        this.todos = res;
-      },
-      error: (error: HttpErrorResponse) => {
-        this.error = error.message;
-      },
-    });
+    this.subscription.add(
+      this.todoService.getTodos().subscribe({
+        next: res => {
+          this.todos = res;
+        },
+        error: (error: HttpErrorResponse) => {
+          this.error = error.message;
+        },
+      })
+    );
   }
 
   createTodo() {
     const randomNumber = Math.floor(Math.random() * 100);
     const title = 'angular - ' + randomNumber;
-    this.todoService.createTodo(title).subscribe({
-      next: res => {
-        this.todos.unshift(res.data.item);
-      },
-      error: (error: HttpErrorResponse) => {
-        this.error = error.message;
-      },
-    });
+    this.subscription.add(
+      this.todoService.createTodo(title).subscribe({
+        next: res => {
+          this.todos.unshift(res.data.item);
+        },
+        error: (error: HttpErrorResponse) => {
+          this.error = error.message;
+        },
+      })
+    );
   }
 
   deleteTodo(todoId: string) {
-    this.todoService.deleteTodo(todoId).subscribe({
-      next: () => {
-        this.todos = this.todos.filter(el => el.id !== todoId);
-      },
-      error: (error: HttpErrorResponse) => {
-        this.error = error.message;
-      },
-    });
+    this.subscription.add(
+      this.todoService.deleteTodo(todoId).subscribe({
+        next: () => {
+          this.todos = this.todos.filter(el => el.id !== todoId);
+        },
+        error: (error: HttpErrorResponse) => {
+          this.error = error.message;
+        },
+      })
+    );
   }
 }
